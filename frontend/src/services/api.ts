499fcab8f737c47603ea3a253055a391272175ee
Medallion13 <nickcar13@gmail.com>
2026-02-01 19:43:44 -0500

    feat(quote): Creating the api integration with the back to get the quote values and ending the front part for basic quotes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,9 +1,25 @@
+import axios from "axios";
+
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
+const client = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
 export const api = {
-  async healthCheck() {
-    const response = await fetch(`${API_URL}/health`);
-    if (!response.ok) throw new Error("Health check fail");
-    return response.json();
+  healthCheck: async () => {
+    const response = await client.get("/health");
+    return response.data;
+  },
+
+  getQuote: async (amount: number) => {
+    const amountFactor = Math.round(amount * 100);
+    const response = await client.post("/api/payment/quote", {
+      amount: amountFactor,
+    });
+    return response.data;
   },
 };
